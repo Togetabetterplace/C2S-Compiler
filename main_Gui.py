7c440cf79c3f1bc229e1092fd6a99662b352830a
Zichen <2155053519@qq.com>
2024-06-15 15:38:30 +0800

    new

--- main_Gui.py
+++ main_Gui.py
@@ -1,12 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFileDialog, QLineEdit, QLabel, QMessageBox
+import os
+import networkx as nx
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout,
+                             QWidget, QFileDialog, QLineEdit, QLabel, QMessageBox, QDialog)
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from asm import to_asm
 from exe import to_exe
 from generate import creat_mcode
 from get_Ptable import grammars, show_tables
-from LR import analysis
-import os
+from LR import analysis, Node
 from lexer import Word_List
+import numpy as np
 
 head = """
 --------------------------+
@@ -20,6 +26,86 @@ phelp = """+-------------------------------------------------+
 - Grammar为语法表                           
 +-------------------------------------------------+
 """
+
+
+class SyntaxTreeDialog(QDialog):
+    def __init__(self, tree, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Syntax Tree Visualization")
+        self.setGeometry(100, 100, 900, 900)
+
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.canvas)
+        self.setLayout(self.layout)
+
+        self.visualize_tree(tree)
+
+    def visualize_tree(self, tree):
+        ax = self.figure.add_subplot(111)
+        ax.clear()
+
+        def plot_tree(node, x, y, level=1):
+            ax.text(x, y, node.type, ha='center', va='center', bbox=dict(
+                facecolor='skyblue', edgecolor='black', boxstyle='round,pad=0.3'))
+            num_children = len(node.child)
+            width = 5 / np.sqrt(np.sqrt(np.sqrt(np.sqrt(np.sqrt(level)))))  # \frac{2.5}{e^{-4+x}+0.5}-1
+            child_x = x - (num_children - 1) * width / \
+                2  # starting x position for children
+            child_y = y - 1  # vertical distance between levels
+
+            for child in node.child:
+                ax.plot([x, child_x], [y, child_y], 'k-')
+                plot_tree(child, child_x, child_y, level + 1)
+                child_x += width  # move to the next child position
+
+        plot_tree(tree, 0, 0)  # start at the origin
+        ax.set_title('Syntax Tree')
+        ax.axis('off')
+        self.canvas.draw()
+
+# class SyntaxTreeDialog(QDialog):
+#     def __init__(self, tree, parent=None):
+#         super().__init__(parent)
+#         self.setWindowTitle("Syntax Tree Visualization")
+#         self.setGeometry(100, 100, 800, 600)
+
+#         self.figure = Figure()
+#         self.canvas = FigureCanvas(self.figure)
+#         self.layout = QVBoxLayout()
+#         self.layout.addWidget(self.canvas)
+#         self.setLayout(self.layout)
+
+#         self.visualize_tree(tree)
+
+#     def visualize_tree(self, tree):
+#         ax = self.figure.add_subplot(111)
+#         G = nx.DiGraph()
+#         self._add_nodes_edges(G, tree)
+
+#         pos = nx.spring_layout(G)
+#         nx.draw(G, pos, with_labels=True, node_size=3000, node_color='skyblue',
+#                 font_size=10, font_weight='bold', arrows=True, ax=ax)
+#         ax.set_title('Syntax Tree')
+
+#         self.canvas.draw()
+
+#     def _add_nodes_edges(self, G, tree, parent=None):
+#         if isinstance(tree, Node):
+#             node_label = str(tree.type)
+#             if parent is None:
+#                 G.add_node(node_label)
+#             else:
+#                 G.add_node(node_label)
+#                 G.add_edge(parent, node_label)
+#             for ch in tree.child[:]:
+#                 self._add_nodes_edges(G, ch, node_label)
+#         else:
+#             node_label = str(tree.type)
+#             if parent is not None:
+#                 G.add_node(node_label)
+#                 G.add_edge(parent, node_label)
 
 
 class PCCCompilerGUI(QMainWindow):
@@ -117,9 +203,8 @@ class PCCCompilerGUI(QMainWindow):
         else:
             self.text_edit.append("\tNo file selected")
 
-
     def generate_exe(self):
-        if self.select_file:
+        if self.selected_file:
             try:
                 to_exe(self.selected_file)
                 name = self.selected_file.split("/")[-1]
@@ -129,26 +214,12 @@ class PCCCompilerGUI(QMainWindow):
                 self.text_edit.append(f"\t编译失败: {e}")
         else:
             self.text_edit.append("\tNo file selected")
-    
+
     def show_mcode(self):
         if self.selected_file:
             mid = creat_mcode(self.selected_file)['mid_code']
             for m in mid:
                 self.text_edit.append(str(m))
-        else:
-            self.text_edit.append("\tNo file selected")
-
-    def show_syntax_tree(self):
-        if self.selected_file:
-            w_list = Word_List(self.selected_file)
-            word_table = w_list.word_list
-            root = analysis(word_table, True)
-            if root[0]:
-                reply = QMessageBox.question(
-                    self, 'Print Syntax Tree', "是否打印语法树(左边为父节点，右边为子节点)?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-                if reply == QMessageBox.Yes:
-                    self.text_edit.append(str(root[1]))
-                    self.text_edit.append("\n\n语法树打印完成")
         else:
             self.text_edit.append("\tNo file selected")
 
@@ -172,22 +243,31 @@ class PCCCompilerGUI(QMainWindow):
         for k in follow_table:
             self.text_edit.append(k)
             self.text_edit.append(str(follow_table[k]))
-        # print(first_table)
         self.text_edit.append("\n预测表:\n")
         for k in predict_table:
             self.text_edit.append(k)
             self.text_edit.append(str(predict_table[k]))
-        # self.text_edit.append
 
     def show_grammar(self):
         for g in grammars:
             self.text_edit.append(f"{g}: {grammars[g]}")
 
+    def show_syntax_tree(self):
+        if self.selected_file:
+            w_list = Word_List(self.selected_file)
+            word_table = w_list.word_list
+            root = analysis(word_table, True)
+            if root[0]:
+                dialog = SyntaxTreeDialog(root[1], self)
+                dialog.exec_()
+        else:
+            self.text_edit.append("\tNo file selected")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     gui = PCCCompilerGUI()
-    gui.resize(900,700)
+    gui.resize(900, 700)
     gui.show()
     gui.text_edit.append(head)
     sys.exit(app.exec_())
